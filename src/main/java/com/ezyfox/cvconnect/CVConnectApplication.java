4990d42035d23a9289a695098e4cbff349d4e298
conght11 <conght11@ghtk.co>
2022-07-15 10:31:23 +0700

    init

---
+++ src/main/java/com/ezyfox/cvconnect/CVConnectApplication.java
@@ -0,0 +1,13 @@
+package com.ezyfox.cvconnect;
+
+import com.tvd12.ezyhttp.server.boot.EzyHttpApplicationBootstrap;
+import com.tvd12.ezyhttp.server.core.asm.RequestHandlerImplementer;
+
+public class CVConnectApplication {
+
+    public static void main(String[] args) throws Exception {
+        RequestHandlerImplementer.setDebug(true);
+        EzyHttpApplicationBootstrap.start(CVConnectApplication.class);
+    }
+
+}
---
+++ src/main/java/com/ezyfox/cvconnect/controller/PingController.java
@@ -0,0 +1,16 @@
+package com.ezyfox.cvconnect.controller;
+
+import com.tvd12.ezyhttp.core.response.ResponseEntity;
+import com.tvd12.ezyhttp.server.core.annotation.Controller;
+import com.tvd12.ezyhttp.server.core.annotation.DoGet;
+import lombok.Setter;
+
+@Setter
+@Controller("/api/v1/ping")
+public class PingController {
+
+    @DoGet("/")
+    public ResponseEntity ping(){
+        return ResponseEntity.ok("Ping");
+    }
+}
