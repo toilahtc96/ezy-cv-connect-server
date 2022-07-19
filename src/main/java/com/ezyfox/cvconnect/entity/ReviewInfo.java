786e79326b6f06b57ced877fc5ae42aec1cc99f8
toilahtc <toilahtc@gmail.com>
2022-07-19 23:07:27 +0700

    update interceptor

--- src/main/java/com/ezyfox/cvconnect/entity/ReviewInfo.java
+++ src/main/java/com/ezyfox/cvconnect/entity/ReviewInfo.java
@@ -1,8 +1,23 @@
 package com.ezyfox.cvconnect.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "review")
+@EqualsAndHashCode(of = "review", callSuper = false)
 public class ReviewInfo extends CommonEntity{
 
+  @Id
   private long id;
   private String description;
   private long star;
--- src/main/java/com/ezyfox/cvconnect/interceptor/GlobalRequestInterceptor.java
+++ src/main/java/com/ezyfox/cvconnect/interceptor/GlobalRequestInterceptor.java
@@ -4,32 +4,40 @@ import com.ezyfox.cvconnect.annotation.UserId;
 import com.ezyfox.cvconnect.exception.TokenNotFoundException;
 import com.ezyfox.cvconnect.services.IAuthenticationService;
 import com.tvd12.ezyfox.bean.annotation.EzyAutoBind;
+import com.tvd12.ezyfox.collect.Sets;
 import com.tvd12.ezyfox.util.EzyLoggable;
 import com.tvd12.ezyhttp.core.annotation.Interceptor;
-import com.tvd12.ezyhttp.core.constant.HttpMethod;
 import com.tvd12.ezyhttp.server.core.interceptor.RequestInterceptor;
-import com.tvd12.ezyhttp.server.core.manager.RequestURIManager;
 import com.tvd12.ezyhttp.server.core.request.RequestArguments;
+import com.ezyfox.cvconnect.controller.PingController;
 
 import java.lang.reflect.Method;
+import java.util.Set;
 
 @Interceptor
 public class GlobalRequestInterceptor
         extends EzyLoggable
         implements RequestInterceptor {
 
-    @EzyAutoBind
-    private RequestURIManager requestUriManager;
+
+    private final Set<Method> authorizedMethods;
     @EzyAutoBind
     private IAuthenticationService authenticationService;
 
+    public GlobalRequestInterceptor() {
+        try {
+            authorizedMethods = Sets.newHashSet(
+                    PingController.class.getDeclaredMethod("ping")
+            );
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
     @Override
     public boolean preHandle(RequestArguments arguments, Method handler) throws Exception {
         logger.info("request uri: {}", arguments.getRequest().getRequestURI());
-        final HttpMethod method = arguments.getMethod();
-        final String uri = arguments.getUriTemplate();
 
-        if (!requestUriManager.isAuthenticatedURI(method, uri)) {
+        if (!authorizedMethods.contains(handler)) {
             return true;
         }
         String accessToken = arguments.getParameter("accessToken");
