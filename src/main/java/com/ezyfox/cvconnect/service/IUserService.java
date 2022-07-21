27f92bc1362b98e25cf10875442dca59a0f8a992
conght11 <conght11@ghtk.co>
2022-07-21 17:50:24 +0700

    fix review

--- src/main/java/com/ezyfox/cvconnect/service/IUserService.java
+++ src/main/java/com/ezyfox/cvconnect/service/IUserService.java
@@ -1,9 +1,7 @@
 package com.ezyfox.cvconnect.service;
 
-import com.ezyfox.cvconnect.entity.User;
 import com.ezyfox.cvconnect.request.LoginRequest;
 import com.ezyfox.cvconnect.request.RegisterRequest;
-import com.ezyfox.cvconnect.response.ResponseEntityData;
 
 public interface IUserService {
 
